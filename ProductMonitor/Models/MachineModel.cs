abe0bbbd838a34af63edac3d86e6bddb8365f9d9
Jiahuan Zeng <3526708355@qq.com>
2025-12-21 16:03:07 +0800

    Initial commit:ProductMonitor project

---
+++ ProductMonitor/Models/MachineModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductMonitor.Models
+{
+    /// <summary>
+    /// 机台数据模型
+    /// </summary>
+    internal class MachineModel
+    {
+        /// <summary>
+        /// 机器名称
+        /// </summary>
+        public string MachineName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public string Statue { get; set; }
+
+        /// <summary>
+        /// 计划数量
+        /// </summary>
+        public int PlanCount { get; set; }
+
+        /// <summary>
+        /// 完成数量
+        /// </summary>
+        public int FinishCount { get; set; }
+
+        /// <summary>
+        /// 工单编号
+        /// </summary>
+        public String OrderNo { get; set; }
+
+        /// <summary>
+        /// 完成百分比（只读）
+        /// </summary>
+        public double Percent { 
+            get
+            {
+                return FinishCount * 100.0 / PlanCount;
+            }
+        }
+
+    }
+}
---
+++ ProductMonitor/UserControls/WorkShopDetailUC.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductMonitor.UserControls
+{
+    /// <summary>
+    /// WorkShopDetailUC.xaml 的交互逻辑
+    /// </summary>
+    public partial class WorkShopDetailUC : UserControl
+    {
+        public WorkShopDetailUC()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 打开详情页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+            detail.Visibility = Visibility.Visible;
+
+            //位移
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation(new Thickness(0,50,0,-50),new Thickness(0,0,0,0),new TimeSpan(0,0,0,400));
+
+            //透明度
+            DoubleAnimation doubleAnimation = new DoubleAnimation(0, 1, new TimeSpan(0, 0, 0, 0, 400));
+
+            Storyboard.SetTarget(thicknessAnimation, detailContent);
+            Storyboard.SetTarget(doubleAnimation, detailContent);
+            Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Margin"));
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("Opacity"));
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(thicknessAnimation);
+            storyboard.Children.Add(doubleAnimation);
+
+            storyboard.Begin();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
